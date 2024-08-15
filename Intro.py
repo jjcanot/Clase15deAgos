cc98c7d625a2965a0ab60699acf153ae2679f7cd
jjcanot <jjcanot@eafit.edu.co>
2024-08-15 07:32:37 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -5,4 +5,21 @@ st.title("PINTUPRO")
 
 st.header("Soluciones creativas a tu alcance")
 st.write("Contactanos al 300 691 6726")
-image = Image.open()
+image = Image.open('Pintupro.png')
+
+
+
+
+
+st. writw("El texto escrito es", texto)
+
+st.subheader("Ahora usemos 2 columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Esta es la primera columna")
+    st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+    resp = st.checkbox('Estoy de acuerdo')
+    if resp:
+       st.write('Correcto!')
