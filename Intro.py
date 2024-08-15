c2b1ed8bb0dae5497a1317ee1f84e263f32b68a5
jjcanot <jjcanot@eafit.edu.co>
2024-08-15 07:26:27 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from PIL import Image
+
 st.title("PINTUPRO")
 
 st.header("Soluciones creativas a tu alcance")
 st.write("Contactanos al 300 691 6726")
+image = Image.open()
